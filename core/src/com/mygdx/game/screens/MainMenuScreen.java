126935a983931aa274f00a0b947d47625feeadbd
Max Bause <max.bause@live.de>
2018-07-18 21:06:37 +0200

    Populated Game Settings Menu Settings need to be persisted and moved to game screen

--- core/src/com/mygdx/game/screens/MainMenuScreen.java
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -5,12 +5,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -28,6 +30,7 @@ public class MainMenuScreen implements Screen {
    private Camera cam;
    private Environment environment;
    private Engine engine;
+   private ShapeRenderer shapeRenderer;
 
 
    //UI
@@ -38,10 +41,16 @@ public class MainMenuScreen implements Screen {
     public MainMenuScreen(final MyGdxGame game) {
         this.game = game;
         this.skin = new Skin(Gdx.files.internal("Skins/default/uiskin.json"));
+        this.shapeRenderer = new ShapeRenderer();
         setBGM();
         createBackground();
-        buildStage();
+        setStage(new MainMenuStage(this,game,skin));
         music.play();
+    }
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+        Gdx.input.setInputProcessor(stage);
     }
 
     private void createBackground() {
@@ -64,26 +73,28 @@ public class MainMenuScreen implements Screen {
         engine = new Engine();
         engine.addEntity(EntityFactory.createGameBoard(0, 50, 0));
         engine.addSystem(new RenderSystem(batch, environment, cam));
-    }
+
+
+
+}
 
     private void setBGM() {
         music = Gdx.audio.newMusic(Gdx.files.internal("BGM/main_menu.wav"));
         music.setLooping(true);
     }
 
-    private void buildStage() {
-        this.stage = new MainMenuStage(music,game,skin);
+    public void stopBGM(){
+        this.music.stop();
     }
+
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0f,0f,0f,1f);
-
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(),
                 Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT |
@@ -91,9 +102,17 @@ public class MainMenuScreen implements Screen {
         batch.begin(cam);
         updateBoardCamera();
         engine.update(delta);
-        stage.act();
-        stage.draw();
         batch.end();
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(new Color(0, 0, 0, 0.6f));
+        shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        shapeRenderer.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
+        stage.draw();
+
 
     }
 
@@ -103,8 +122,6 @@ public class MainMenuScreen implements Screen {
         cam.lookAt(0, 0, 0);
         cam.rotateAround(new Vector3(0, 0, 0), new Vector3(0, 1, 0), 0.1f);
 
-        //amount to pan up/down along the Y axis
-        //cam.position.add(0, panY * PAN_Y_SPEED, 0);
         cam.update();
     }
 
